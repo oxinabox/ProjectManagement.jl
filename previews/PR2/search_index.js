e509acd46c33b9bb3b9627a84c25903e85ad5ad5
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-28 16:39:34 +0000

    build based on 8d4ffd4

---
+++ previews/PR2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ProjectManagement.jl-1","page":"Home","title":"ProjectManagement.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [ProjectManagement]","category":"page"},{"location":"#ProjectManagement.PertBeta","page":"Home","title":"ProjectManagement.PertBeta","text":"PertBeta(a, b, c) <: ContinuousUnivariateDistribution\n\nThe PERT Beta distribution.\n\na: the minimum value of the support\nb: the mode\nc: the maximum value of the support\n\n\n\n\n\n","category":"type"}]
+}
